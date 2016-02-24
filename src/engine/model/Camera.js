09cfcaf34da5a1fbc4768e6acc288334b050200b
jeremythuff <huff@library.tamu.edu>
2016-02-23 23:44:33 -0600

    Window is resizable

--- src/engine/model/Camera.js
+++ src/engine/model/Camera.js
@@ -1,5 +1,18 @@
 export class Camera {
 	constructor( fov, aspect, near, far) {
-		return new THREE.PerspectiveCamera( fov, aspect, near, far );
+		
+		this.PerspectiveCamera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+
+		this.registerResizeEvent();
+
+		return this.PerspectiveCamera;
+	}
+
+	registerResizeEvent() {
+		let camera = this;
+		window.addEventListener('resize', function(){
+			camera.PerspectiveCamera.aspect = window.innerWidth / window.innerHeight;
+	    	camera.PerspectiveCamera.updateProjectionMatrix();
+		});
 	}
 }
