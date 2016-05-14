84c5ecf788dea456a8a5e0d943499596e0103e09
jeremythuff <huff@library.tamu.edu>
2016-05-14 02:07:14 -0500

    WIP login registration

--- src/game/states/login/login.js
+++ src/game/states/login/login.js
@@ -5,62 +5,62 @@ let Login = new State("Login");
 
 Login.init(function() {
 
-    console.log("Login init");
+	console.log("Login init");
 
 });
 
 Login.load(function() {
-    if(Login.loaded) return;
-    
-    Login.context.form = {
-        email: {
-            value: "",
-            validation: function(elem) {
-                let regEx = /\S+@\S+\.\S+/;
-                if(regEx.test(elem.value)) {
-                    elem.classList.remove("invalid");
-                    elem.classList.add("valid");
-                } else {
-                    elem.classList.remove("valid");
-                    elem.classList.add("invalid");
-                }
+	if(Login.loaded) return;
+	
+	Login.context.form = {
+		email: {
+			value: "",
+			validation: function(elem) {
+				let regEx = /\S+@\S+\.\S+/;
+				if(regEx.test(elem.value)) {
+					elem.classList.remove("invalid");
+					elem.classList.add("valid");
+				} else {
+					elem.classList.remove("valid");
+					elem.classList.add("invalid");
+				}
 
-                if(elem.value.length === 0) {
-                    elem.classList.remove("valid");
-                    elem.classList.remove("invalid");
-                }
-            }
-        },
-        password: {
-            value: "",
-            validation: function(elem) {
-                if(elem.value.length>3) {
-                    elem.classList.remove("invalid");
-                    elem.classList.add("valid");
-                } else {
-                    elem.classList.remove("valid");
-                    elem.classList.add("invalid");
-                }
+				if(elem.value.length === 0) {
+					elem.classList.remove("valid");
+					elem.classList.remove("invalid");
+				}
+			}
+		},
+		password: {
+			value: "",
+			validation: function(elem) {
+				if(elem.value.length>3) {
+					elem.classList.remove("invalid");
+					elem.classList.add("valid");
+				} else {
+					elem.classList.remove("valid");
+					elem.classList.add("invalid");
+				}
 
-                if(elem.value.length === 0) {
-                    elem.classList.remove("valid");
-                    elem.classList.remove("invalid");
-                }
-            }
-        },
-        login: function() {
-            console.log(Login.context.form.email.value);
-            console.log(Login.context.form.password.value);
-        },
-        register: function() {
-            console.log("Register");
-        }
-    }
+				if(elem.value.length === 0) {
+					elem.classList.remove("valid");
+					elem.classList.remove("invalid");
+				}
+			}
+		},
+		login: function() {
+			console.log(Login.context.form.email.value);
+			console.log(Login.context.form.password.value);
+		},
+		register: function() {
+			console.log("Register");
+		}
+	}
 
-    Login.gui.addView("Login", "src/game/states/login/gui/templates/loginForm.hbs");
+	Login.gui.addView("Login", "src/game/states/login/gui/templates/loginForm.hbs");
 
-    console.log(Login);
-    console.log("Login loaded");
+	console.log(Login);
+	console.log("Login loaded");
 });
 
 Login.update(function() {}); // Will take pass delta as arg if needed
@@ -68,11 +68,11 @@ Login.update(function() {}); // Will take pass delta as arg if needed
 Login.render(function() {}); // Will take pass delta as arg if needed
 
 Login.close(function() {
-    console.log("Login close");
+	console.log("Login close");
 });
 
 Login.destroy(function() {
-    console.log("Login destroy");
+	console.log("Login destroy");
 });
 
 export {Login};
