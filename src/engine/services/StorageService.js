baf75667987e9d13e5c4b3e0e5610eb60b2ad1a0
jeremythuff <huff@library.tamu.edu>
2016-05-14 01:55:59 -0500

    added simple storage service

---
+++ src/engine/services/StorageService.js
@@ -0,0 +1,41 @@
+
+class StorageService {
+    constructor(storageType) {
+
+        let StorageService = this;
+
+        StorageService._storage = typeof storageType === undefined ? storageType : sessionStorage;
+
+        window.addEventListener('storage', function(e) {
+            console.log(e);
+        });
+
+    };
+
+    set(key, value) {
+
+
+        if(typeof value == "object") {
+            value = JSON.stringify(value);
+        }
+        
+        this._storage.setItem(key, value);
+    };
+
+    get(key) {
+
+        let value = this._storage.getItem(key);
+
+        try { value = JSON.parse(value); } catch(e) {}
+
+        return value;
+    }
+
+    remove(key) {
+        delete this._storage[key];
+    }
+}
+
+let storageService = new StorageService();
+
+export { storageService };
